c1cff68a89b7b9c687c07400e40816dc8af0572b
ciubotaru <vitalie@ciubotaru.tk>
2013-10-02 00:41:34 +0900

    fixed typo and updates uninstall

--- trunk/uninstall.php
+++ trunk/uninstall.php
@@ -1,6 +1,7 @@
 <?php
 
-if(!defined(WP_UNINSTALL_PLUGIN) exit();
+if(!defined('WP_UNINSTALL_PLUGIN')) exit();
 delete_option('share-on-diaspora-settings');
+delete_option('share-on-diaspora-settings2');
 
 ?>
