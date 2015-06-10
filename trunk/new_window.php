cf8d7c0dab7555ded5b10a5dcad698a992291848
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2015-06-11 01:08:23 +0900

    Fixed js formatting in new_window.php

--- trunk/new_window.php
+++ trunk/new_window.php
@@ -40,191 +40,255 @@ div.bot_opt{position:fixed;bottom:0;height:1.6em;border-top:1px solid #eee;left:
 </style>
 
 <script type="text/javascript">
-function extras(a)
-  {
-  var b=document.getElementById("remember").checked;
-  var c=document.getElementById("markdown").checked;
-  var d=document.getElementById("shorten").checked;
-  var e=document.getElementById("norem").checked;
-  if(b)
-    {
-    localStorage["remember"]="true";
-    if(e)
-      {
-      localStorage["lastPod"]=a
-      }
-    }
-  else
-    {
-    localStorage["remember"]="false"
-    }
-  if(d)
-    {
-    var f=new XMLHttpRequest;
-    f.open("GET","http://api.bitly.com/v3/shorten?login=bartimeo&apiKey=R_5fe8386a052e3f3d6ece604eab0c59db&format=txt&domain=j.mp&longUrl="+url);
-    f.onreadystatechange=function()
-      {
-      if(f.readyState==4)
-        {
-        if(f.status==200)
-          {
-          console.log(f.responseText);
-          url=f.responseText
-          }
-        else
-          {
-          console.log("Problem?",f)
-          }
+function extras(a) {
+    var b = document.getElementById("remember").checked;
+    var c = document.getElementById("markdown").checked;
+    var d = document.getElementById("shorten").checked;
+    var e = document.getElementById("norem").checked;
+    if (b) {
+        localStorage["remember"] = "true";
+        if (e) {
+            localStorage["lastPod"] = a
         }
-      };
-    f.send()
-  }
-  if(!e)
-    {
-    recuerda(a)
+    } else {
+        localStorage["remember"] = "false"
     }
-  else
-    {
-    localStorage["forget"]="true"
-    }
-  return true
-  }
-
-function forget(a)
-  {
-  localStorage.removeItem("lastPod");
-  localStorage.removeItem("lastPod2");
-  localStorage.removeItem("lastPod3");
-  var b=document.getElementsByClassName("hidepod");
-  for(i=0;i<b.length;i++)
-    {
-    b[i].className="dpod"
-    }
-  var c=document.getElementsByClassName("lastpod");
-  for(j=0;j<c.length;j++)
-    {
-    c[j].parentNode.removeChild(c[j])
-    }
-  if((document.getElementsByClassName("hidepod")||document.getElementsByClassName("lastpod"))&&!a)
-    {
-    forget("again")
-    }
-  }
-
-function share(a)
-  {
-  if(a!=="")
-    {
-    extras(a);
-    var b="http://"+a+"/bookmarklet?url="+encodeURIComponent(url)+"&title="+encodeURIComponent(title);
-    if(notes!=="")
-      {
-      b+="&notes="+encodeURIComponent(notes)
-      }
-    b+="&jump=doclose";
-    location.href=b
-    }
-  else
-    {
-    document.getElementById("podurl").className="error"
-    }
-  }
-
-function recuerda(a)
-  {
-  if(localStorage["lastPod"]&&localStorage["lastPod"]!==a)
-    {
-    if(localStorage["lastPod2"]&&localStorage["lastPod2"]!==a)
-      {
-      localStorage["lastPod3"]=localStorage["lastPod2"]
-      }
-    localStorage["lastPod2"]=localStorage["lastPod"]
-    }
-  localStorage["lastPod"]=a;
-  return true
-  }
-
-function updlinks()
-  {
-  var a=document.getElementsByClassName("dpod");
-  for(i=0;i<a.length;i++)
-    {
-    var b="http://"+a[i].title+"/bookmarklet?url="+encodeURIComponent(url)+"&title="+encodeURIComponent(title);
-    if(notes!=="")
-      {
-      b+="&notes="+encodeURIComponent(notes)
-      }
-    b+="&jump=doclose";
-    a[i].href=b;
-    a[i].onclick=function()
-      {
-      extras(this.title)
-      }
-    }
-  }
-
-function crealinks()
-  {
-  var a=document.getElementsByClassName("dpod");
-  if(localStorage["forget"]==="true")
-    {
-    document.getElementById("norem").checked="checked"
-    }
-  else
-    {
-    if(localStorage["lastPod"])
-      {
-      var b=localStorage["lastPod"];
-      var c=document.createElement("a");
-      c.className="dpod lastpod";
-      c.title=b;
-      if(document.getElementsByTagName("body")[0].innerText)
-        {
-        c.innerText=b
-        }
-      else
-        {
-        c.textContent=b
-        }
-      for(i=0;i<a.length;i++)
-        {
-        if(a[i].title===b)
-          {
-          a[i].className="dpod hidepod"
-          }
-        }
-      document.getElementById("podlist").insertBefore(c,document.getElementById("first"));
-      if(localStorage["lastPod2"])
-        {
-        var b=localStorage["lastPod2"];
-        var c=document.createElement("a");
-        c.className="dpod lastpod";
-        c.title=b;
-        if(document.getElementsByTagName("body")[0].innerText)
-          {
-          c.innerText=b
-          }
-        else
-          {
-          c.textContent=b
-          }
-        for(i=0;i<a.length;i++)
-          {
-          if(a[i].title===b)
-            {
-            a[i].className="dpod hidepod"
+    if (d) {
+        var f = new XMLHttpRequest;
+        f.open("GET", "http://api.bitly.com/v3/shorten?login=bartimeo&apiKey=R_5fe8386a052e3f3d6ece604eab0c59db&format=txt&domain=j.mp&longUrl=" + url);
+        f.onreadystatechange = function() {
+            if (f.readyState == 4) {
+                if (f.status == 200) {
+                    console.log(f.responseText);
+                    url = f.responseText
+                } else {
+                    console.log("Problem?", f)
+                }
             }
-          }
-        document.getElementById("podlist").insertBefore(c,document.getElementById("first"));
-        if(localStorage["lastPod3"])
-          {
-          var b=localStorage["lastPod3"];
-          var c=document.createElement("a");
-          c.className="dpod lastpod";
-          c.title=b;
-          if(document.getElementsByTagName("body")[0].innerText)
-            {
-            c.innerText=b}else{c.textContent=b}for(i=0;i<a.length;i++){if(a[i].title===b){a[i].className="dpod hidepod"}}document.getElementById("podlist").insertBefore(c,document.getElementById("first"))}}}}updlinks();document.getElementById("openao").onclick=function(){document.getElementById("contopt").className="showopt";document.getElementById("openao").style.display="none"};document.getElementById("delete").onclick=function(){forget()};document.getElementById("markdown").onchange=function(){if(document.getElementById("markdown").checked){title="["+title+"]("+url+")";url=" "}else{title=oldtit;url=oldurl}updlinks()};document.getElementById("shorten").onchange=function(){if(document.getElementById("shorten").checked){var a=new XMLHttpRequest;a.open("GET","http://api.bitly.com/v3/shorten?login=bartimeo&apiKey=R_5fe8386a052e3f3d6ece604eab0c59db&format=txt&domain=j.mp&longUrl="+url);a.onreadystatechange=function(){if(a.readyState==4){if(a.status==200){console.log(a.responseText);shurl=decodeURIComponent(encodeURIComponent(a.responseText).replace("%0A",""));if(document.getElementById("markdown").checked){title="["+oldtitle+"]("+shurl+")";url=" "}else{url=shurl}updlinks()}else{console.log("Problem?",a)}}};a.send()}else{if(document.getElementById("markdown").checked){title="["+oldtitle+"]("+oldurl+")";url=" "}else{title=oldtit;url=oldurl}updlinks()}}}function redirect(){if(title===""&&url===""){document.getElementsByTagName("body")[0].innerHTML="";location.href="about"}else{if(localStorage["remember"]&&localStorage["remember"]==="true"&&localStorage["lastPod"]&&redir!=="false"){document.getElementsByTagName('body')[0].innerHTML="Sharing <b>"+title+"</b> ("+url+") to "+localStorage["lastPod"];var a="http://"+localStorage["lastPod"]+"/bookmarklet?url="+encodeURIComponent(url)+"&title="+encodeURIComponent(title);if(notes!==""){a+="&notes="+encodeURIComponent(notes)}a+="&jump=doclose";location.href=a;return true}else{if(document.getElementsByTagName("body")[0].innerText){document.getElementById("sharetitle").innerText=title;document.getElementById("shareurl").innerText=url}else{document.getElementById("sharetitle").textContent=title;document.getElementById("shareurl").textContent=url}crealinks();return false}}}function par(a){var b="[\\?&]"+a+"=([^&]*)";var c=new RegExp(b);var d=window.location.href;var e=c.exec(d);if(e==null)return"";else return e[1]}var title=decodeURIComponent(par("title"));var url=decodeURIComponent(par("url"));var notes=decodeURIComponent(par("notes"));var redir=decodeURIComponent(par("redirect"));var oldtit=title;var oldurl=url;var oldnot=notes;window.onload=function(){redirect()}
+        };
+        f.send()
+    }
+    if (!e) {
+        recuerda(a)
+    } else {
+        localStorage["forget"] = "true"
+    }
+    return true
+}
+
+function forget(a) {
+    localStorage.removeItem("lastPod");
+    localStorage.removeItem("lastPod2");
+    localStorage.removeItem("lastPod3");
+    var b = document.getElementsByClassName("hidepod");
+    for (i = 0; i < b.length; i++) {
+        b[i].className = "dpod"
+    }
+    var c = document.getElementsByClassName("lastpod");
+    for (j = 0; j < c.length; j++) {
+        c[j].parentNode.removeChild(c[j])
+    }
+    if ((document.getElementsByClassName("hidepod") || document.getElementsByClassName("lastpod")) && !a) {
+        forget("again")
+    }
+}
+
+function share(a) {
+    if (a !== "") {
+        extras(a);
+        var b = "http://" + a + "/bookmarklet?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title);
+        if (notes !== "") {
+            b += "&notes=" + encodeURIComponent(notes)
+        }
+        b += "&jump=doclose";
+        location.href = b
+    } else {
+        document.getElementById("podurl").className = "error"
+    }
+}
+
+function recuerda(a) {
+    if (localStorage["lastPod"] && localStorage["lastPod"] !== a) {
+        if (localStorage["lastPod2"] && localStorage["lastPod2"] !== a) {
+            localStorage["lastPod3"] = localStorage["lastPod2"]
+        }
+        localStorage["lastPod2"] = localStorage["lastPod"]
+    }
+    localStorage["lastPod"] = a;
+    return true
+}
+
+function updlinks() {
+    var a = document.getElementsByClassName("dpod");
+    for (i = 0; i < a.length; i++) {
+        var b = "http://" + a[i].title + "/bookmarklet?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title);
+        if (notes !== "") {
+            b += "&notes=" + encodeURIComponent(notes)
+        }
+        b += "&jump=doclose";
+        a[i].href = b;
+        a[i].onclick = function() {
+            extras(this.title)
+        }
+    }
+}
+
+function crealinks() {
+    var a = document.getElementsByClassName("dpod");
+    if (localStorage["forget"] === "true") {
+        document.getElementById("norem").checked = "checked"
+    } else {
+        if (localStorage["lastPod"]) {
+            var b = localStorage["lastPod"];
+            var c = document.createElement("a");
+            c.className = "dpod lastpod";
+            c.title = b;
+            if (document.getElementsByTagName("body")[0].innerText) {
+                c.innerText = b
+            } else {
+                c.textContent = b
+            }
+            for (i = 0; i < a.length; i++) {
+                if (a[i].title === b) {
+                    a[i].className = "dpod hidepod"
+                }
+            }
+            document.getElementById("podlist").insertBefore(c, document.getElementById("first"));
+            if (localStorage["lastPod2"]) {
+                var b = localStorage["lastPod2"];
+                var c = document.createElement("a");
+                c.className = "dpod lastpod";
+                c.title = b;
+                if (document.getElementsByTagName("body")[0].innerText) {
+                    c.innerText = b
+                } else {
+                    c.textContent = b
+                }
+                for (i = 0; i < a.length; i++) {
+                    if (a[i].title === b) {
+                        a[i].className = "dpod hidepod"
+                    }
+                }
+                document.getElementById("podlist").insertBefore(c, document.getElementById("first"));
+                if (localStorage["lastPod3"]) {
+                    var b = localStorage["lastPod3"];
+                    var c = document.createElement("a");
+                    c.className = "dpod lastpod";
+                    c.title = b;
+                    if (document.getElementsByTagName("body")[0].innerText) {
+                        c.innerText = b
+                    } else {
+                        c.textContent = b
+                    }
+                    for (i = 0; i < a.length; i++) {
+                        if (a[i].title === b) {
+                            a[i].className = "dpod hidepod"
+                        }
+                    }
+                    document.getElementById("podlist").insertBefore(c, document.getElementById("first"))
+                }
+            }
+        }
+    }
+    updlinks();
+    document.getElementById("openao").onclick = function() {
+        document.getElementById("contopt").className = "showopt";
+        document.getElementById("openao").style.display = "none"
+    };
+    document.getElementById("delete").onclick = function() {
+        forget()
+    };
+    document.getElementById("markdown").onchange = function() {
+        if (document.getElementById("markdown").checked) {
+            title = "[" + title + "](" + url + ")";
+            url = " "
+        } else {
+            title = oldtit;
+            url = oldurl
+        }
+        updlinks()
+    };
+    document.getElementById("shorten").onchange = function() {
+        if (document.getElementById("shorten").checked) {
+            var a = new XMLHttpRequest;
+            a.open("GET", "http://api.bitly.com/v3/shorten?login=bartimeo&apiKey=R_5fe8386a052e3f3d6ece604eab0c59db&format=txt&domain=j.mp&longUrl=" + url);
+            a.onreadystatechange = function() {
+                if (a.readyState == 4) {
+                    if (a.status == 200) {
+                        console.log(a.responseText);
+                        shurl = decodeURIComponent(encodeURIComponent(a.responseText).replace("%0A", ""));
+                        if (document.getElementById("markdown").checked) {
+                            title = "[" + oldtitle + "](" + shurl + ")";
+                            url = " "
+                        } else {
+                            url = shurl
+                        }
+                        updlinks()
+                    } else {
+                        console.log("Problem?", a)
+                    }
+                }
+            };
+            a.send()
+        } else {
+            if (document.getElementById("markdown").checked) {
+                title = "[" + oldtitle + "](" + oldurl + ")";
+                url = " "
+            } else {
+                title = oldtit;
+                url = oldurl
+            }
+            updlinks()
+        }
+    }
+}
+
+function redirect() {
+    if (title === "" && url === "") {
+        document.getElementsByTagName("body")[0].innerHTML = "";
+        location.href = "about"
+    } else {
+        if (localStorage["remember"] && localStorage["remember"] === "true" && localStorage["lastPod"] && redir !== "false") {
+            document.getElementsByTagName('body')[0].innerHTML = "Sharing <b>" + title + "</b> (" + url + ") to " + localStorage["lastPod"];
+            var a = "http://" + localStorage["lastPod"] + "/bookmarklet?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title);
+            if (notes !== "") {
+                a += "&notes=" + encodeURIComponent(notes)
+            }
+            a += "&jump=doclose";
+            location.href = a;
+            return true
+        } else {
+            if (document.getElementsByTagName("body")[0].innerText) {
+                document.getElementById("sharetitle").innerText = title;
+                document.getElementById("shareurl").innerText = url
+            } else {
+                document.getElementById("sharetitle").textContent = title;
+                document.getElementById("shareurl").textContent = url
+            }
+            crealinks();
+            return false
+        }
+    }
+}
+
+function par(a) {
+    var b = "[\\?&]" + a + "=([^&]*)";
+    var c = new RegExp(b);
+    var d = window.location.href;
+    var e = c.exec(d);
+    if (e == null) return "";
+    else return e[1]
+}
+var title = decodeURIComponent(par("title"));
+var url = decodeURIComponent(par("url"));
+var notes = decodeURIComponent(par("notes"));
+var redir = decodeURIComponent(par("redirect"));
+var oldtit = title;
+var oldurl = url;
+var oldnot = notes;
+window.onload = function() {
+    redirect()
+}
 </script>
 </head>
 <body>
