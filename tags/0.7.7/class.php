93aec67fc959457be359014473833cd3efc3c5a7
Vitalie Ciubotaru <vitalie@ciubotaru.tokyo>
2019-05-17 00:22:34 +0900

    Tagging 0.7.7

--- tags/0.7.7/class.php
+++ tags/0.7.7/class.php
@@ -31,7 +31,7 @@ class ShareOnDiaspora {
 		wp_enqueue_script( 'share-on-diaspora' );
 	}
 
-	public static $button_defaults = array(
+	public $button_defaults = array(
 	'button_color' => '3c72c2',
 	'button_background' => 'ecf2f6',
 	'button_color_hover' => '3c72c2',
@@ -41,14 +41,14 @@ class ShareOnDiaspora {
 	'button_text' => 'share this'
 	);
 
-	public static $image_defaults = array(
+	public $image_defaults = array(
 	'image_file' => '',
 	'use_own_image' => '0'
 	);
 
-	public static $podlist_defaults = array( 'podlist' => array( 'example.com' => '1') );
+	public $podlist_defaults = array( 'podlist' => array( 'example.com' => '1') );
 
-	public static $color_profiles = array(
+	public $color_profiles = array(
 	'Vitalie' => array(
 		'button_color' => '3b5998',
 		'button_background' => 'eceef5',
@@ -87,13 +87,13 @@ class ShareOnDiaspora {
 		)
 	);
 
-	public static $plugin_version = array( 'version' => SHARE_ON_DIASPORA_VERSION );
+	public $plugin_version = array( 'version' => SHARE_ON_DIASPORA_VERSION );
 
 	//public $podlist_update_url = 'http://the-federation.info/pods.json';
-	public static $podlist_update_url = 'http://podupti.me/api.php?format=json&key=4r45tg';
+	public $podlist_update_url = 'http://podupti.me/api.php?format=json&key=4r45tg';
 
 
-	public static function generate_button($preview, $use_own_image, $is_feed) {
+	public function generate_button($preview, $use_own_image, $is_feed) {
 		/**
 		 * if preview == TRUE && $use_own_image == '0', prepare fake link and output standard button
 		 * if preview == FALSE && $use_own_image == '0', prepare real link and output standard button
@@ -101,7 +101,7 @@ class ShareOnDiaspora {
 		 * if preview == TRUE && $use_own_image == '1', impossible
 		 * the button is inside, so let's prepare button first
 		 */
-		$button_defaults = ShareOnDiaspora::$button_defaults;
+		$button_defaults = $this->button_defaults;
 		$options_array = get_option( 'share-on-diaspora-settings' );
 		if ( $use_own_image ) {
 			//use own image
